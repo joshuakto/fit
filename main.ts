6603dac6398f76ab2f419152a60e8fb696a0db01
joshuakto <34743132+joshuakto@users.noreply.github.com>
2024-02-28 23:23:35 +0800

    feat: enabled pulling of remote content to local repo

--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
+import { RestEndpointMethodTypes } from '@octokit/plugin-rest-endpoint-methods';
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, TFile } from 'obsidian';
 import { Add } from 'src/add';
 import { Fit } from 'src/fit';
@@ -26,17 +27,17 @@ export default class MyPlugin extends Plugin {
 	fit: Fit;
 	add = new Add(this.app.vault.adapter)
 	
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	async createTreeNodeFromFile({path, type, extension}: {path: string, type: string, extension?: string}): Promise<any> {
+	async createTreeNodeFromFile(
+		{path, type, extension}: {path: string, type: string, extension?: string}): 
+		Promise<RestEndpointMethodTypes["git"]["createTree"]["parameters"]["tree"][number]> {
 		if (type === "deleted") {
 			return {
-				path: path,
+				path,
 				mode: '100644',
 				type: 'blob',
 				sha: null
-			}	
+			}
 		}
-		// temp function of stageFile: check if file exists in vault data adapter
 		let encoding: string;
 		let content: string 
 		if (extension && ["pdf", "png", "jpeg"].includes(extension)) {
@@ -52,6 +53,7 @@ export default class MyPlugin extends Plugin {
 			encoding = 'utf-8'
 			content = await this.app.vault.adapter.read(path)
 		}
+		// temp function of stageFile: check if file exists in vault data adapter
 		const fileExists = this.add.stageFile(path)
 		if (!fileExists) {
 			throw new Error("Unexpected error: attempting to createBlob for non-existent file, please file an issue on github with info to reproduce the issue.");
@@ -61,7 +63,6 @@ export default class MyPlugin extends Plugin {
 			repo: this.settings.repo,
 			content, encoding
 		})
-		console.log(`blob sha: ${path} -- ${blob.data.sha}`)
 		return {
 			path: path,
 			mode: '100644',
@@ -81,40 +82,151 @@ export default class MyPlugin extends Plugin {
 		)
 	}
 
+	checkSettingsConfigured(): boolean {
+		if (["<Personal-Access-Token> ", ""].includes(this.settings.pat)) {
+			new Notice("Please provide git personal access tokens in Fit settings and try again.")
+			return false
+		}
+		if (["<Github-Username>", ""].includes(this.settings.owner)) {
+			new Notice("Please provide git repo owner in Fit settings and try again.")
+			return false
+		}
+		if (["<Repository-Name>", ""].includes(this.settings.repo)) {
+			this.settings.repo = `obsidian-${this.app.vault.getName()}-storage`
+		}
+		return true
+	}
+
+	compareSha(remoteSha: {[k:string]:string}, lastFetchedRemoteSha: {[k:string]:string}): Array<{path: string, status: 'added' | 'removed' | 'changed'}> {
+		const allPaths = Array.from(new Set([...Object.keys(remoteSha), ...Object.keys(lastFetchedRemoteSha)]));
+	
+		return allPaths.reduce<{path: string, status: 'added' | 'removed' | 'changed'}[]>((changes, path) => {
+			const inRemote = path in remoteSha;
+			const inLastFetched = path in lastFetchedRemoteSha;
+	
+			if (inRemote && !inLastFetched) {
+				changes.push({ path, status: 'added' });
+			} else if (!inRemote && inLastFetched) {
+				changes.push({ path, status: 'removed' });
+			} else if (inRemote && inLastFetched && remoteSha[path] !== lastFetchedRemoteSha[path]) {
+				changes.push({ path, status: 'changed' });
+			}
+			// Unchanged files are implicitly handled by not adding them to the changes array
+	
+			return changes;
+		}, []);
+	}
+
 	async onload() {
 		await this.loadSettings();
-		this.fit = new Fit("testRepo", this.settings)
+		this.fit = new Fit(this.settings)
 
-		// This creates an icon in the left ribbon.
+		// pull remote to local
+		this.addRibbonIcon('github', 'Fit pull', async (evt: MouseEvent) => {
+			if (!this.checkSettingsConfigured()) {
+				return
+			}
+			// https://dev.to/lucis/how-to-push-files-programatically-to-a-repository-using-octokit-with-typescript-1nj0
+			const {data: latestRemoteCommit} = await this.fit.getRef(`heads/${this.settings.branch}`)
+			const latestRemoteCommitSha = latestRemoteCommit.object.sha;
+			const pluginLocalStore = await this.loadData()
+			if (latestRemoteCommitSha == pluginLocalStore.lastFetchedCommitSha) {
+				new Notice("Local copy already up to date")
+				return
+			}
+			
+			// Since remote changes are detected, get the latest remote tree
+			const { data: latestRemoteTree } = await this.fit.getTree(latestRemoteCommitSha)
+			const remoteSha = Object.fromEntries(latestRemoteTree.tree.map((node) : [string, string] | null=>{
+				// currently ignoreing directory changes
+				if (node.type=="blob") {
+					if (!node.path || !node.sha) {
+						throw new Error("path and sha not found for blob node in remote");
+					}
+					return [node.path, node.sha]
+				}
+				return null
+			}).filter(entry => entry!==null) as [string, string][])
+			
+			// TODO checked for changed files by iterating and comparing the remote sha
+			let addToLocal: Record<string, string> = {};
+			let deleteFromLocal: Array<string> = [];
+			// if lastFetchedRemoteSha is not in local store yet, update every file
+			if (!pluginLocalStore.lastFetchedRemoteSha) {
+				const entries = await Promise.all(Object.entries(remoteSha).map(async (kv) => {
+					const path = kv[0]
+					const file_sha = kv[1]
+					const {data} = await this.fit.getBlob(file_sha)
+					const contentUtf8 = atob(data.content)
+					return {[path]: contentUtf8}
+				}))
+				addToLocal = Object.assign({}, ...entries)
+			} else {
+				const remoteChanges = this.compareSha(remoteSha, pluginLocalStore.lastFetchedRemoteSha)
+				await Promise.all(remoteChanges.map(async change => {
+					if (["changed", "added"].includes(change.status)) {
+						const {data} = await this.fit.getBlob(remoteSha[change.path])
+						const decodedContent = atob(data.content);
+						addToLocal = {...addToLocal, [change.path]: decodedContent}
+					} else {
+						deleteFromLocal = [...deleteFromLocal, change.path]
+					}
+				}))
+			}
+
+			// Update changed files locally TODO: need to check if there are clashing local changes, if so, prompt above for user confirmation before proceeding
+			// const remoteBlob = await this.fit.octokit.rest.git.getBlob({
+			// 	owner: this.fit.owner,
+			// 	repo: this.fit.repo,
+			// 	file_sha: remoteSha['test.md']
+			// })
+			// const decodedContent = atob(remoteBlob.data.content);
+			// this.app.vault.create('test.md', decodedContent)
+			// this.app.vault.modify()
+			const filewriting = Object.entries(addToLocal).map(async entry=>{
+				const path = entry[0]
+				const content = entry[1]
+				const file = this.app.vault.getFileByPath(path)
+				if (file) {
+					await this.app.vault.modify(file, content)
+					return {path, type: "modification"}
+				} else {
+					await this.app.vault.create(path, content)
+					return {path, type: "creation"}
+				}
+			})
+			const deletion = deleteFromLocal.map(async f=> {
+				const file = this.app.vault.getFileByPath(f)
+				if (file) { 
+					await this.app.vault.delete(file)
+					return {path: file.path, type: "deletion"}
+				}
+			}).filter(Boolean) as Promise<{path: string, type: string}>[]
+			const fileOps = await Promise.all([...filewriting, ...deletion])
+			pluginLocalStore.lastFetchedCommitSha = latestRemoteCommitSha
+			this.saveData({...pluginLocalStore, lastFetchedRemoteSha: {...remoteSha}})
+			fileOps.map(op=> new Notice(`${op.path} ${op.type} completed.`))
+		});
+
+		// push local to remote
 		const ribbonIconEl = this.addRibbonIcon('github', 'Fit', async (evt: MouseEvent) => {
 			const pluginLocalStore = await this.loadData()
-			if (this.settings.pat === "<Personal-Access-Token>") {
-				new Notice("Please provide git personal access tokens in Fit settings and try again.")
+			// if (pluginLocalStore.latestCommitTreeSha && pluginLocalStore.latestCommitTreeSha !=)
+			if (!this.checkSettingsConfigured()) {
 				return
-			}
-			if (this.settings.owner === "<Github-Username>") {
-				new Notice("Please provide git repo owner in Fit settings and try again.")
-				return
-			}
-			if (this.settings.repo === "<Repository-Name>") {
-				this.settings.repo = `obsidian-${this.app.vault.getName()}-storage`
 			}
 			const owner = this.settings.owner
 			const repo = this.settings.repo
 			// https://dev.to/lucis/how-to-push-files-programatically-to-a-repository-using-octokit-with-typescript-1nj0
-			const latestCommit = await this.fit.octokit.rest.git.getRef({
-				owner, repo,
-				ref: `heads/${this.settings.branch}`
-			})
-			const latestCommitSha = latestCommit.data.object.sha;
-			const latestCommitData = await this.fit.octokit.rest.git.getCommit({
-				owner, repo,
-				commit_sha: latestCommitSha
-			})
-
+			const {data: latestRef} = await this.fit.getRef(`heads/${this.settings.branch}`)
+			const latestRemoteCommitSha = latestRef.object.sha;
+			if (latestRemoteCommitSha != pluginLocalStore.lastFetchedCommitSha) {
+				new Notice("Remote changed after last pull/write, please pull again.")
+				return
+			}
+			const {data: latestCommit} = await this.fit.getCommit(latestRemoteCommitSha)
+			
 			const files = this.app.vault.getFiles()
-			console.log(files)
-			new Notice("test sha")
 			const localSha = await this.computeLocalSha()
 
 			let changedFiles: Array<{path: string, type: string, extension?: string}>;
@@ -132,9 +244,7 @@ export default class MyPlugin extends Plugin {
 				changedFiles = files.map(f=> {return {
 					path: f.path, type: 'changed', extension: f.extension}})
 			}
-			const { data: latestRemoteTree } = await this.fit.octokit.rest.git.getTree({
-				owner, repo, tree_sha: latestCommitSha, recursive: 'true',
-			});
+			const { data: latestRemoteTree } = await this.fit.getTree(latestRemoteCommitSha)
 			const removedFiles:Array<{path: string, type: string}>  = latestRemoteTree.tree.flatMap(node=> {
 				if(!(node.type == "tree") && node.path && !Object.keys(localSha).includes(node.path)) {
 					return {path: node.path, type: 'deleted'}
@@ -143,32 +253,23 @@ export default class MyPlugin extends Plugin {
 			})
 			changedFiles.push(...removedFiles)
 			if (changedFiles.length == 0) {
-				new Notice("Local copy already up to date.")
+				new Notice("No local changes detected.")
 				return
 			}
-
-			this.saveData({...this.settings, localSha: {...localSha}})
 
 			const treeNodes = await Promise.all(changedFiles.map((f) => {
 				return this.createTreeNodeFromFile(f)
 			}))
-			const newTree = await this.fit.octokit.rest.git.createTree({
-				owner, repo,
-				tree: treeNodes,
-				base_tree: latestCommitData.data.tree.sha
-			})
+
+			const newTree = await this.fit.createTree(treeNodes, latestCommit.tree.sha)
 			const message = `Commit from ${this.settings.deviceName} on ${new Date().toLocaleString()}`
-			const newCommit = await this.fit.octokit.rest.git.createCommit({
+			const {data: newCommit} = await this.fit.octokit.rest.git.createCommit({
 				owner, repo, message,
 				tree: newTree.data.sha,
-				parents: [latestCommitSha]
+				parents: [latestRemoteCommitSha]
 			})
-			const updatedRef = await this.fit.octokit.rest.git.updateRef({
-				owner, repo,
-				ref: `heads/${this.settings.branch}`,
-				sha: newCommit.data.sha
-			})
-			console.log(updatedRef.data.url)
+			await this.fit.updateRef(`heads/${this.settings.branch}`, newCommit.sha)
+			await this.saveData({...this.settings, localSha: {...localSha}, lastFetchedCommitSha: newCommit.sha})
 			new Notice(`Successful pushed to ${this.settings.repo} (${message})`)
 		});
 		// Perform additional things with the ribbon
@@ -217,12 +318,6 @@ export default class MyPlugin extends Plugin {
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
 
 		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
 		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
--- src/fit.ts
+++ src/fit.ts
@@ -1,23 +1,31 @@
+import { RestEndpointMethodTypes } from "@octokit/plugin-rest-endpoint-methods"
 import { MyPluginSettings } from "main"
 import { Octokit } from "octokit"
 
 export interface IFit {
+    owner: string
     repo: string
+    branch: string
+    deviceName: string
     octokit: Octokit
-    auth: string | undefined
     fileSha1: (path: string) => Promise<string>
+    getTree: (tree_sha: string) => Promise<RestEndpointMethodTypes["git"]["getTree"]["response"]>
 }
 
 export class Fit implements IFit {
+    owner: string
     repo: string
-    octokit: Octokit
     auth: string | undefined
+    branch: string
+    deviceName: string
+    octokit: Octokit
 
-    constructor(repo: string, setting: MyPluginSettings) {
-        // this.auth for debugging, remove when publishing plugin
-        this.auth = setting.pat
+    constructor(setting: MyPluginSettings) {
+        this.owner = setting.owner
+        this.repo = setting.repo
+        this.branch = setting.branch
+        this.deviceName = setting.deviceName
         this.octokit = new Octokit({auth: setting.pat})
-        this.repo = repo
     }
 
     async fileSha1(fileContent: string): Promise<string> {
@@ -26,5 +34,67 @@ export class Fit implements IFit {
         const hashArray = Array.from(new Uint8Array(hashBuf));
         const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
         return hashHex;
+    }
+
+    async getRef(ref: string): Promise<RestEndpointMethodTypes["git"]["getRef"]["response"]> {
+        return this.octokit.rest.git.getRef({
+            owner: this.owner,
+            repo: this.repo,
+            ref,
+            // Hack to disable caching which leads to inconsistency for read after write https://github.com/octokit/octokit.js/issues/890
+            headers: {
+                "If-None-Match": ''
+            }
+        })
+    }
+
+    async getCommit(commit_sha: string): Promise<RestEndpointMethodTypes["git"]["getCommit"]["response"]> {
+        return this.octokit.rest.git.getCommit({
+            owner: this.owner,
+            repo: this.repo,
+            commit_sha,
+            // Hack to disable caching which leads to inconsistency for read after write https://github.com/octokit/octokit.js/issues/890
+            headers: {
+                "If-None-Match": ''
+            }
+        })
+    }
+
+    async getTree(tree_sha: string): Promise<RestEndpointMethodTypes["git"]["getTree"]["response"]> {
+        const tree =  await this.octokit.rest.git.getTree({
+            owner: this.owner,
+            repo: this.repo,
+            tree_sha,
+            recursive: 'true'
+        })
+        return tree
+    }
+
+    async createTree(
+        treeNode: RestEndpointMethodTypes["git"]["createTree"]["parameters"]["tree"], base_tree_sha: string): 
+        Promise<RestEndpointMethodTypes["git"]["createTree"]["response"]> {
+        return await this.octokit.rest.git.createTree({
+            owner: this.owner,
+            repo: this.repo,
+            tree: treeNode,
+            base_tree: base_tree_sha
+        })
+    }
+
+    async updateRef(ref: string, sha: string): Promise<RestEndpointMethodTypes["git"]["updateRef"]["response"]> {
+        return await this.octokit.rest.git.updateRef({
+            owner: this.owner,
+            repo: this.repo,
+            ref,
+            sha
+        })
+    }
+
+    async getBlob(file_sha:string): Promise<RestEndpointMethodTypes["git"]["getBlob"]["response"]> {
+        return await this.octokit.rest.git.getBlob({
+            owner: this.owner,
+            repo: this.repo,
+            file_sha
+        })
     }
 }
